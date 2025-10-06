a56269919faf2510838cea4db1b4e530695f1b89
duongvi1k <duongvi1k2@gmail.com>
2025-10-06 09:50:48 +0700

    behavioral full option

---
+++ DPM225500_DuongTriVi_VD10_Command/RemoteControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225500_DuongTriVi_VD10_Command
+{
+    internal class RemoteControl
+    {
+    }
+}
